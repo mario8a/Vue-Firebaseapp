66f14753173abcc5d67d17cfc70f35fc23d64abe
Mario Ochoa <mario8ato@hotmail.com>
2019-06-11 22:51:02 -0500

    lista de tareas

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,122 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app" class="container">
+		<h1>Vue and Firebase</h1>
+		<hr>
+		
+		
+		<div class="card">
+			<div class="card-header">
+				<h3>Add a Link</h3>
+			</div>
+			<div class="card-body">
+				<form v-on:submit.prevent="addLink"  class="form-inline">
+
+					<div class="form-group">
+						<label for="">Title</label>
+						<input
+							placeholder="Title"
+							v-model="newLink.title"
+							type="text" class="form-control mx-sm-4"/>
+					</div>
+					
+					<div class="form-group">
+						<label for="">Author</label>
+						<input
+							placeholder="Author"
+							v-model="newLink.author"
+							type="text" class="form-control mx-sm-4"/>
+					</div>
+					
+					<div class="form-group">
+						<label for="">Url</label>
+						<input
+							placeholder="Title"
+							v-model="newLink.url"
+							type="text" class="form-control mx-sm-4"/>
+					</div>
+
+					<input type="submit" class="btn btn-success" value="Add Link">
+				</form>
+			</div>
+		</div>
+
+		<hr>
+
+		<div class="card">
+			<div class="card-header">
+				<h3 class="card-title">Links List</h3>
+			</div>
+			<div class="card-block">
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>User</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="link in links">
+							<td>
+								<a v-bind:href="link.url">
+									{{ link.title }}
+								</a>
+							</td>
+							<td>{{ link.author }}</td>
+							<td>
+								<button 
+									v-on:click="deleteLink(link)"
+									class="btn btn-danger">
+									<i class="fa fa-trash-o" aria-hidden="true"></i>
+								</button>									
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Firebase from 'firebase'
+
+let config = {
+    //config de firebase
+};
+
+let app = Firebase.initializeApp(config);
+let db  = app.database();
+
+//creando base de datos
+let linksRef = db.ref('links');
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	firebase: {
+		links: linksRef
+	},
+	data() {
+		return {
+			newLink: {
+				title: '',
+				author: '',
+				url: ''
+			}
+		}
+	},
+	methods: {
+		addLink: function() {
+			linksRef.push(this.newLink);
+			this.newLink.title = '';
+			this.newLink.author = '';
+			this.newLink.url = '';
+		},
+		deleteLink: function(link) {
+			linksRef.child(link['.key']).remove();
+		}
+	}
 }
 </script>
 
